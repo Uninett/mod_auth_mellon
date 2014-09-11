47a767d5f37d1d3a1c004abbf8bb80d1b7eab328
Olav Morken <olav.morken@uninett.no>
2014-09-11 15:51:32 +0200

    Ensure that all generated session IDs are cryptographically strong.
    We used to call RAND_pseudo_bytes, which will generate cryptographically
    strong random bytes in most cases. Under memory pressure or other error
    conditions, it can return weak random bytes.
    
    It is unlikely that there is a way for an attacker to set up a condition
    where the user is sucessfully logged in but receives a weak session
    identifier, but to be safe, we switch to RAND_bytes.

--- auth_mellon_util.c
+++ auth_mellon_util.c
@@ -789,17 +789,12 @@ int am_check_url(request_rec *r, const char *url)
 int am_generate_random_bytes(request_rec *r, void *dest, apr_size_t count)
 {
     int rc;
-    rc = RAND_pseudo_bytes((unsigned char *)dest, (int)count);
-    if(rc == -1) {
+    rc = RAND_bytes((unsigned char *)dest, (int)count);
+    if(rc != 1) {
         ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r,
                       "Error generating random data: %lu",
                       ERR_get_error());
         return HTTP_INTERNAL_SERVER_ERROR;
-    }
-
-    if(rc == 0) {
-        ap_log_rerror(APLOG_MARK, APLOG_WARNING, 0, r,
-                      "Random data is not cryptographically strong.");
     }
 
     return OK;
